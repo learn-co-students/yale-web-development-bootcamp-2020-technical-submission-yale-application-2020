d1b1992dce87fffcdcad92a8cbafa41869255ce7
Lauren Velez <laurenmiavelez@gmail.com>
2020-01-19 01:10:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Lauren"
+age = 19
+favorite_food = "chicken"
+height_in_inches = 64
+height_message = AVERAGE_HEIGHT_IN_INCHES > height_in_inches ? "You are not taller than average." : "You are taller than average."
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)

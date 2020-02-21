61c36d5b1db446f9d5345828f43b96eb4b672291
Zihe Zhao <zihe.zhao@outlook.com>
2020-02-21 21:35:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,6 +8,9 @@ age = 19
 favorite_food = "chocolate"
 height_in_inches = 67
 height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? ("You are taller than average.") : ("You are not taller than average.")
+favorite_food = "Chocolate"
+height_in_inches = 68
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? ("You are taller than average") : ("You are not taller than average")
 
 ## Do not modify below this line!
 

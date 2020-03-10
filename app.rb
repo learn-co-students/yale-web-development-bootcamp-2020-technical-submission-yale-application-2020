c9831e0c21282de66f2de303f8124c456dbf14bb
Courtney DeNaut <courtney.denaut@yale.edu>
2020-03-10 22:47:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Courtney DeNaut"
+age = 20
+favorite_food = "French Fries"
+height_in_inches = 69
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? ("You are taller than average.") : ("You are not taller than average.")
 
 ## Do not modify below this line!
 

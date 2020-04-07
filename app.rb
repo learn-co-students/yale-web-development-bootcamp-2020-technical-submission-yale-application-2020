7a2aac84d6f9d6daa382af1d76354157967d4068
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-04-07 20:39:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Kaleb"
+age = 18
+favorite_food = "pepperoni pizza"
+height_in_inches = 71
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES)? "You are taller than average." : "You are not taller than average."
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)

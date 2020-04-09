951a6d6887bfcd29d40437fb0c42b4445c897b0c
Ethan Coyle <ethan.coyle@yale.edu>
2020-04-09 20:04:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ name = "Ethan Coyle"
 age = 19
 favorite_food = "pizza"
 height_in_inches = 74
-height_message = (height_in_inches < AVERAGE_HEIGHT_IN_INCHES) ? "You are not taller than average." : "You are taller than average."
+height_message = (height_in_inches <= AVERAGE_HEIGHT_IN_INCHES) ? "You are not taller than average." : "You are taller than average."
 
 ## Do not modify below this line!
 

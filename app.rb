30674baaea79d7cccc871ade197b2021ebc5854e
Jeacy Espinoza <jay.espinoza@yale.edu>
2020-04-15 02:13:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Jeacy"
+age = 19
+favorite_food = "buffalo wings"
+height_in_inches = 66
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
 
 ## Do not modify below this line!
 

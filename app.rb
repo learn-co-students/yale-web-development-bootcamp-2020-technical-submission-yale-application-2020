1fa37caed5bb8408905298520255d90d9d26f08e
Zi Xin Ou <eric.ou@yale.edu>
2020-04-15 02:43:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Eric Ou"
+age = 21
+favorite_food = "Sashimi"
+height_in_inches = 70.0
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average." : "You are not taller than average."
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)

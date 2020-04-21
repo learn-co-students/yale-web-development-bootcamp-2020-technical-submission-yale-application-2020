f0834485585af707ba2d246625de4073ae8cda11
Arthur Hwang <arthur.hwang@yale.edu>
2020-04-21 08:34:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,22 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+# 1
+name = "Arthur Hwang"
+
+# 2
+age = 23
+
+# 3
+favorite_food = "pizza"
+
+# 4
+height_in_inches = 71
+
+# 5
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES)? ("You are taller than average."):("You are not taller than average.") 
+
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)

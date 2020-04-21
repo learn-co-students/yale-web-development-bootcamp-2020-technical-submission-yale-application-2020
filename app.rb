3dc44797e5f5501ef1bafd27ee5ada403550ea20
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-04-21 16:27:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
-require_relative './test'
+#require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Jonas Kavaliauskas"
+age = 23
+favorite_food = "burger"
+height_in_inches = 70.8661
+average_male_height_in_inches = 66.9291
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average." : "You are not taller than average."
+
 ## Do not modify below this line!
 
-output(name, age, favorite_food, height_message)
+#output(name, age, favorite_food, height_message)

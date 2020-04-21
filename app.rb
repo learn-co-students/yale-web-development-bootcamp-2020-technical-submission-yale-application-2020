329adc583551051f71fd7a7a3df121ce7580c54a
Enrique Valencia <enrique.valencia@yale.edu>
2020-04-21 21:07:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
-# Your Code Here!
-
+name = "Enrique Valencia"
+age = 19
+favorite_food = "crabs" 
+height_in_inches = 73
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average" : "You are not taller than average"
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)

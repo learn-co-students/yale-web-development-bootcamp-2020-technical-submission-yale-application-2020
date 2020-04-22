a8ba5bbb92d9511a51c4a9647e21ebe0c3891337
Chloe Hong <chloe.hong@yale.edu>
2020-04-22 09:30:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Chloe"
+age = 20
+favorite_food = "fried chicken"
+height_in_inches = 62
+
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
 
 ## Do not modify below this line!
 

8d923c7218cdb7ba955fb178299067f666b9fb1b
Adil Gondal <adil.gondal@yale.edu>
2020-04-22 05:52:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Adil"
+age = 18
+favorite_food = "pizza"
+height_in_inches = 70
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
 
 ## Do not modify below this line!
 

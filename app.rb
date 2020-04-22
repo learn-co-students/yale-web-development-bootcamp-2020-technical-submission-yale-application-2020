cc9dbcaca5d2d44f11609dd7d2a06e5f8e8826ed
David Yu <david.yu@yale.edu>
2020-04-22 16:36:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
-# Your Code Here!
+name = "Dave"
+age = 19
+favorite_food = "Spaghetti alla carbonara"
+height_in_inches = (12 * 5) + 11
+height_message = "You are #{(height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "taller" : "not taller"} than average."
 
 ## Do not modify below this line!
 

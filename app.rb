309502bfedb0aa392f84c27e16b76ca750bf3a52
Peiyao Ma <mapeiyao@gmail.com>
2020-04-24 20:56:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Jack Ma"
+age = 20
+favorite_food = "Sea Urchin Ceviche"
+height_in_inches = 69
+
+taller_than_average = height_in_inches > 67
+
+height_message = taller_than_average ? "You are taller than average." : "You are not taller than average."
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)

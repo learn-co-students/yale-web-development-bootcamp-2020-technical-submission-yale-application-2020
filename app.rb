ea26bd21a160b40deb458bfacc0e91525055ef53
David Lee <david.lee.sl2634@yale.edu>
2020-05-02 20:58:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require_relative './test'
-AVERAGE_HEIGHT_IN_INCHES = 67
 
-# Your Code Here!
+AVERAGE_HEIGHT_IN_INCHES = 67 
+name = "David"
+age = 20
+favorite_food = "Steak"
+height_in_inches = 69
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? ("You are taller than average.") : ("You are not taller than average.")
+
 
 ## Do not modify below this line!
 
